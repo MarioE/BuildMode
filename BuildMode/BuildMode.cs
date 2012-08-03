8938118bc132c2fb11a83f19e7de0363337b03a0
MarioE <MarioE27@hotmail.com>
2012-08-03 17:01:02 -0400

    First commit

---
+++ BuildMode/BuildMode.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Hooks;
+using Terraria;
+using TShockAPI;
+
+namespace BuildMode
+{
+    [APIVersion(1, 12)]
+    public class BuildMode : TerrariaPlugin
+    {
+        public override string Author
+        {
+            get { return "MarioE"; }
+        }
+        private bool[] Build = new bool[256];
+        public override string Description
+        {
+            get { return "Adds a command for builders."; }
+        }
+        private DateTime LastCheck = DateTime.UtcNow;
+        public override string Name
+        {
+            get { return "BuildMode"; }
+        }
+        public override Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+
+        public BuildMode(Main game)
+            : base(game)
+        {
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GameHooks.Update -= OnUpdate;
+                NetHooks.GetData -= OnGetData;
+                ServerHooks.Leave -= OnLeave;
+            }
+        }
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command("buildmode", BuildModeCmd, "buildmode"));
+
+            GameHooks.Update += OnUpdate;
+            NetHooks.GetData += OnGetData;
+            ServerHooks.Leave += OnLeave;
+        }
+
+        void OnGetData(GetDataEventArgs e)
+        {
+            if (e.MsgID == PacketTypes.Tile && !e.Handled && Build[e.Msg.whoAmI])
+            {
+                int type = e.Msg.readBuffer[e.Index];
+                if (type == 1 || type == 3)
+                {
+                    Player plr = Main.player[e.Msg.whoAmI];
+                    Item selected = Main.player[e.Msg.whoAmI].inventory[Main.player[e.Msg.whoAmI].selectedItem];
+                    TShock.Players[e.Msg.whoAmI].GiveItem(selected.type, selected.name, plr.width, plr.height, 1);
+                }
+            }
+        }
+        void OnLeave(int plr)
+        {
+            Build[plr] = false;
+        }
+        void OnUpdate()
+        {
+            if ((DateTime.UtcNow - LastCheck).TotalSeconds > 1)
+            {
+                LastCheck = DateTime.UtcNow;
+                bool dayTime = Main.dayTime;
+                double time = Main.time;
+                Main.dayTime = true;
+                Main.time = 27000.0;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    if (Build[i])
+                    {
+                        NetMessage.SendData(7, i);
+                    }
+                }
+                Main.dayTime = dayTime;
+                Main.time = time;
+            }
+        }
+
+        void BuildModeCmd(CommandArgs e)
+        {
+            Build[e.Player.Index] = !Build[e.Player.Index];
+            e.Player.SendMessage(String.Format("{0}abled build mode.", Build[e.Player.Index] ? "En" : "Dis"), Color.Green);
+            NetMessage.SendData(7, e.Player.Index);
+        }
+    }
+}
---
+++ BuildMode/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuildMode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BuildMode")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f41df45b-6513-4b68-8823-6e8557425049")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
